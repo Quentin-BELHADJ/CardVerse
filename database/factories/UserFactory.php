5eb9b7bbf0a7d6c8eb12e788ba587115318bd123
Quentin Belhadj <quentin.belhadj@edu.univ-fcomte.fr>
2026-01-05 10:22:35 +0100

    mistook files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,13 +2,19 @@
 
 namespace Database\Factories;
 
-use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-
-class CategoryFactory extends Factory
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
 {
-    protected $model = Category::class;
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
 
     /**
      * Define the model's default state.
@@ -18,8 +24,21 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            // Utiliser un nombre aléatoire pour le nom de la catégorie
-            'name' => 'Category_' . rand(1, 100) . $this->faker->unique()->word(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
